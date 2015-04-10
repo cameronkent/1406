b34052393dbbf5cfc59a75ba56860a7ab77d95ab
advocaat <adam.nicholls@my.jcu.edu.au>
2015-04-10 18:21:43 +1000

    Update and rename prac.html to index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+  //connect to dbase up in here
+
+?>
+
+
+<!doctype html>
+
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Yep </title>
+<link href="styler.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+<div id="headly">
+  <h1>Home</h1>
+</div>
+<div id-"bodycontent">
+  <div id="nav">
+    <ul>
+      <li><a href"index.php">Home</a></li>
+      <li><a href"#">SomePage</a></li>
+      <li><a href"#">SomePage</a></a></li>
+    </ul>
+  </div>
+  
+</div>
+</body>
+</html>
